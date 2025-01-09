936dce2c40a3fe15d3e0e18858dacbd4812cfd78
Giiacomo <giacomodg02@gmail.com>
2025-01-09 17:10:07 +0100

    first commit

---
+++ src/test/java/com/advancia/stage/jenkins_test_demo/JenkinsTestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.advancia.stage.jenkins_test_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsTestDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
