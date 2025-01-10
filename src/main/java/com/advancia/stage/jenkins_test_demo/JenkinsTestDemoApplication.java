d3bab3037197fd5513f67565bba5b7c4d32a4f30
Giiacomo <giacomodg02@gmail.com>
2025-01-10 09:28:04 +0100

    4

--- src/main/java/com/advancia/stage/jenkins_test_demo/JenkinsTestDemoApplication.java
+++ src/main/java/com/advancia/stage/jenkins_test_demo/JenkinsTestDemoApplication.java
@@ -2,12 +2,18 @@ package com.advancia.stage.jenkins_test_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class JenkinsTestDemoApplication {
+public class JenkinsTestDemoApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(JenkinsTestDemoApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsTestDemoApplication.class, args);
 	}
-
 }
