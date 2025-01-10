700650afb316ef8aede9c0f6c23ea5419f2ba564
Giiacomo <giacomodg02@gmail.com>
2025-01-10 11:49:20 +0100

    controller

--- src/main/java/com/advancia/stage/jenkins_test_demo/controller/Controller.java
+++ src/main/java/com/advancia/stage/jenkins_test_demo/controller/Controller.java
@@ -2,6 +2,8 @@ package com.advancia.stage.jenkins_test_demo.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -11,8 +13,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class Controller {
 
 	@GetMapping("/test")
-	public String getMethodName(@RequestParam String param) {
-		return new String("test");
+	public ResponseEntity<String> getMethodName(@RequestParam String param) {
+		return new ResponseEntity<String>("ciao", HttpStatus.OK);
 	}
 	
 	
