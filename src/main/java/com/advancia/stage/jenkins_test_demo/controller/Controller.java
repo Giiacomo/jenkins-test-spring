8676c73bc22be0fedab5f7df9ea4fae247a05e66
Giiacomo <giacomodg02@gmail.com>
2025-01-10 11:44:41 +0100

    controller

---
+++ src/main/java/com/advancia/stage/jenkins_test_demo/controller/Controller.java
@@ -0,0 +1,19 @@
+package com.advancia.stage.jenkins_test_demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping("/api")
+public class Controller {
+
+	@GetMapping("/test")
+	public String getMethodName(@RequestParam String param) {
+		return new String("test");
+	}
+	
+	
+}
